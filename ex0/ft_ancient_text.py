f4450d115718237fe9cc93b48fc841ab8aa0fc0b
Matthias Voiturier <matthias.voiturier@hotmail.com>
2026-07-03 11:34:08 +0200

    ex0

---
+++ ex0/ft_ancient_text.py
@@ -0,0 +1,30 @@
+import sys
+import typing
+
+
+def main() -> None:
+    if not sys.argv[1:]:
+        print("Usage: ft_ancient_text.py <file>")
+        return
+
+    print("=== Cyber Archives Recovery ===")
+    file_path: str = sys.argv[1]
+    file: typing.IO[str] | None = None
+    print(f"Accessing file '{file_path}'")
+
+    try:
+        file = open(file_path, "r")
+        content: str = file.read()
+        print("---\n\n")
+        print(content)
+        print("\n---")
+    except OSError as err:
+        print(f"Error opening file '{file_path}': {err}")
+    finally:
+        if file is not None:
+            file.close()
+            print(f"File '{file_path}' closed.")
+
+
+if __name__ == "__main__":
+    main()
