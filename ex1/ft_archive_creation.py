49a1b358f6858dd446058efcfefede67ec03e6f5
Fastops07 <matthias.voiturier@hotmail.com>
2026-07-05 12:13:43 +0200

    ex1 wip jusqu a sauver fichier

--- ex1/ft_archive_creation.py
+++ ex1/ft_archive_creation.py
@@ -7,14 +7,15 @@ def main() -> None:
         print("Usage: ft_ancient_text.py <file>")
         return
 
-    print("=== Cyber Archives Recovery ===")
+    print("=== Cyber Archives Recovery & Preservation ===")
     file_path: str = sys.argv[1]
     file: typing.IO[str] | None = None
+    content: str | None = None
     print(f"Accessing file '{file_path}'")
 
     try:
         file = open(file_path, "r")
-        content: str = file.read()
+        content = file.read()
         print("---\n\n")
         print(content)
         print("\n---")
@@ -25,7 +26,25 @@ def main() -> None:
             file.close()
             print(f"File '{file_path}' closed.")
 
+    if content is None:
+        return 
 
+    print("Transform data:")
+    print("---")
+    print()
+
+    transformed_data:list[str] = [line + "#" for line in content.splitlines()]
+    
+    for line in transformed_data:
+        print(line)
+        
+    print("\n---")
+
+    file_name :str = input("Enter new file name (or empty): ")
+    if file_name:
+        print("Not saving data.")
+    # else:
+        # try open write close 
 if __name__ == "__main__":
     main()
 
